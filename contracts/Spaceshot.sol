bf4a120c6e829ab333eaf0e5d0618ae8d7938078
moganesan <developer.moganesan>
2022-10-19 16:34:38 +0530

    comtract function for bedding amount

--- contracts/Spaceshot.sol
+++ contracts/Spaceshot.sol
@@ -4,16 +4,28 @@ pragma solidity ^0.8.9;
 contract Spaceshot {
     mapping(uint256 => address) public players;
     mapping(address => uint256) public balances;
+    mapping(address => bool) public accounts;
+    uint256 public startingTime;
+    uint256 public endingTime;
     uint256 public playersCount;
     uint256 public transactions;
 
-    function createAccount() public returns (bool) {
-        playersCount += 1;
-        players[playersCount] = msg.sender;
-        return true;
+    struct playerDetails {
+        uint256 amount;
+        uint256 multiplier;
+        uint256 beddingTime;
     }
 
+    mapping(uint256 => playerDetails) public currentPlayers;
+
     function deposit() public payable {
+        bool account = accounts[msg.sender];
+        if (!account) {
+            playersCount++;
+            players[playersCount] = msg.sender;
+            accounts[msg.sender] = true;
+        }
+        require(accounts[msg.sender], "User Not have An Account");
         balances[msg.sender] += msg.value;
         transactions++;
     }
@@ -31,5 +43,22 @@ contract Spaceshot {
 
     function getGameBalance() public view returns (uint256) {
         return address(this).balance;
+    }
+
+    function betAmount(uint256 amount, uint256 multiplier) public {
+        uint256 balance = balances[msg.sender];
+        uint256 timestamp = block.timestamp;
+        require(timestamp < endingTime, "Game Starts within 10 seconds");
+        require(balance >= amount, "Insuffecient Funds");
+        currentPlayers[block.timestamp] = playerDetails(
+            amount,
+            multiplier,
+            block.timestamp
+        );
+    }
+
+    function startNewGame() public {
+        startingTime = block.timestamp;
+        endingTime = block.timestamp + 10;
     }
 }
