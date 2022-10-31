dc5a1be12638af6d1145dc7c9a1be14b58ab1df1
moganesan <developer.moganesan>
2022-10-31 16:17:24 +0530

    game contract integration

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,123 @@
 import Head from "next/head";
 import Image from "next/image";
 import Header from "../components/header";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { ethers } from "ethers";
+import crypto from "crypto";
 import GameSpace from "../components/gamespace";
 import MultiPlierHistory from "../components/multiplierHistory";
+import ContractAbi from "../artifacts/contracts/Spaceshot.sol/Spaceshot.json";
 
 export default function Home() {
   const [amount, setAmount] = useState(0);
   const [multiplier, setMultiplier] = useState(0);
+  const [gameHash, setGameHash] = useState("");
+  const [clientSeed, setClientSeed] = useState("");
+  const [startGame, setStartGame] = useState(false);
+  const [multiplierCrash, setMultiplierCrash] = useState(0);
 
+  const incrementBetAmount = () => {
+    setAmount((a) => a + 1);
+  };
+
+  const decrementAmount = () => {
+    setAmount((a) => a - 1);
+  };
+
+  const minBetAmount = () => {
+    setAmount(1);
+  };
+
+  const maxBetAmount = () => {
+    setAmount(50);
+  };
+
+  function crashPointFromHash(serverSeed) {
+    const hash = crypto
+      .createHmac("sha256", serverSeed)
+      .update(clientSeed)
+      .digest("hex");
+
+    const hs = parseInt(100 / 5); // 5% house edge
+    if (divisible(hash, hs)) return 1;
+
+    const h = parseInt(hash.slice(0, 52 / 4), 16);
+    const e = Math.pow(2, 52);
+    return Math.floor((100 * e - h) / (e - h)) / 100.0;
+  }
+
+  function divisible(hash, mod) {
+    var val = 0;
+    var o = hash.length % 4;
+    for (var i = o > 0 ? o - 4 : 0; i < hash.length; i += 4)
+      val = ((val << 16) + parseInt(hash.substring(i, i + 4), 16)) % mod;
+    return val === 0;
+  }
+
+  const getBlockHash = async () => {
+    const contractAddress = "0xD7746beC4f562b4c6eD610417858f97175Fe6854";
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const accounts = await provider.listAccounts();
+    const signer = provider.getSigner();
+    const network = await provider.getNetwork();
+
+    const contract = new ethers.Contract(
+      contractAddress,
+      ContractAbi.abi,
+      signer
+    );
+
+    setClientSeed(contractAddress);
+
+    const gameHash = await contract.getBlockhash();
+    const playerCount = await contract.getPlayerCount();
+
+    console.log("Player Count:", playerCount.toNumber());
+
+    console.log("Game Hash", gameHash);
+    const crash = crashPointFromHash(gameHash);
+
+    console.log("Crash", crash);
+    console.log("Multiplier Crash Vlaue", crash);
+    setMultiplierCrash(parseFloat(crash));
+
+    setGameHash(gameHash);
+  };
   const placeBet = async () => {
-    const contractAddress = "0x54c512d21c71a8f6f6edD45A23fA9196c63f825d";
+    const contractAddress = "0xD7746beC4f562b4c6eD610417858f97175Fe6854";
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const accounts = await provider.listAccounts();
+    const signer = provider.getSigner();
+    const network = await provider.getNetwork();
+
+    const contract = new ethers.Contract(
+      contractAddress,
+      ContractAbi.abi,
+      signer
+    );
+
+    try {
+      console.log(amount, multiplier, multiplierCrash);
+      const contractCall = await contract.betAmount(
+        amount,
+        multiplier,
+        parseInt(multiplierCrash)
+      );
+
+      const res = await contractCall.wait();
+
+      const events = res.events.find((event) => event.event == "returnResult");
+
+      const [_amount, _multiplier, profit] = events.args;
+
+      console.log(events.args);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const endGame = async () => {
+    const contractAddress = "0xD7746beC4f562b4c6eD610417858f97175Fe6854";
     const provider = new ethers.providers.Web3Provider(window.ethereum);
     const accounts = await provider.listAccounts();
     const signer = provider.getSigner();
@@ -26,28 +132,58 @@ export default function Home() {
     );
 
     try {
-      await contract.betAmount(amount, multiplier, {
-        value: ethers.utils.parseUnits(
-          ethers.utils.formatEther(amount),
-          "ether"
-        ),
-      });
-      console.log(contractResponse);
+      await contract.endGame(multiplier);
     } catch (err) {
       console.log(err);
     }
   };
+
+  const getGameResult = async () => {
+    const contractAddress = "0xD7746beC4f562b4c6eD610417858f97175Fe6854";
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const accounts = await provider.listAccounts();
+    const signer = provider.getSigner();
+    const network = await provider.getNetwork();
+
+    console.log("Call Game Status");
+
+    const contract = new ethers.Contract(
+      contractAddress,
+      ContractAbi.abi,
+      signer
+    );
+
+    try {
+      const result = await contract.getGameResult();
+      console.log("Game Result:", result);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    getBlockHash();
+    const gameStartInternal = setInterval(() => {
+      setStartGame((c) => !c);
+      getBlockHash();
+    }, 10000);
+
+    return () => {
+      clearInterval(gameStartInternal);
+    };
+  }, []);
   return (
     <div>
       <Header />
       <MultiPlierHistory />
-      <GameSpace />
+      <GameSpace startgame={startGame} multiplier={multiplierCrash} />
       <div className="px-10">
         <div className="flex">
           <div className="border-2 w-96 border-yellow-400 px-3 py-3 rounded-md flex flex-col">
             <span className="font-VT323 text-gray-300">Amount</span>
             <input
               value={amount}
+              onChange={(e) => setAmount(e.target.value)}
               className="text-white h-10 font-VT323 outline-none bg-transparent px-2 text-2xl"
               type={"number"}
             />
@@ -57,6 +193,7 @@ export default function Home() {
             <div className="flex items-center">
               <span className="font-VT323 text-2xl ml-2">X</span>
               <input
+                onChange={(e) => setMultiplier(e.target.value)}
                 value={multiplier}
                 type={"number"}
                 className="text-white h-10 outline-none bg-transparent font-VT323 text-2xl px-2"
@@ -67,23 +204,43 @@ export default function Home() {
         <div className="mt-5 flex">
           <div className="flex items-center w-96 justify-between">
             <div>
-              <button className="button font-VT323">MIN</button>
+              <button onClick={minBetAmount} className="button font-VT323">
+                MIN
+              </button>
             </div>
             <div>
-              <button className="button font-VT323">-</button>
+              <button onClick={decrementAmount} className="button font-VT323">
+                -
+              </button>
             </div>
             <div>
-              <button className="button font-VT323">+</button>
+              <button
+                onClick={incrementBetAmount}
+                className="button font-VT323"
+              >
+                +
+              </button>
             </div>
             <div>
-              <button className="button font-VT323">MAX</button>
+              <button onClick={maxBetAmount} className="button font-VT323">
+                MAX
+              </button>
             </div>
           </div>
           <div className="ml-20 w-96">
-            <button className="button2">PLACE BET</button>
+            {!startGame ? (
+              <button className="button3" onClick={() => placeBet()}>
+                PLACE BET
+              </button>
+            ) : (
+              <button className="button2 cursor-wait">
+                Wait FOR NEXT ROUND
+              </button>
+            )}
           </div>
         </div>
-        <div className="mt-10">
+
+        {/* <div className="mt-10">
           <span className="font-VT323 px-10 border-2 py-5 border-yellow-400">
             ALL PLAYERS
           </span>
@@ -111,7 +268,9 @@ export default function Home() {
               </tr>
             </tbody>
           </table>
-        </div>
+        </div> */}
+
+        <div className="mt-20" />
       </div>
     </div>
   );
