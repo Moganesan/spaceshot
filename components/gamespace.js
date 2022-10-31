25d17e9991beeb2f97d39bfcfb66729f623f2efd
moganesan <developer.moganesan>
2022-10-31 16:17:54 +0530

    game animations

--- components/gamespace.js
+++ components/gamespace.js
@@ -2,21 +2,28 @@ import anime from "animejs";
 import { useRef, useState, useEffect } from "react";
 import SpaceCraft from "./spacecraft";
 import { ethers } from "ethers";
+import RocketAnimation from "../public/rocketAnimation.json";
+import Lottie from "lottie-react";
 import ContractAbi from "../artifacts/contracts/Spaceshot.sol/Spaceshot.json";
 
-const GameSpace = () => {
+const GameSpace = ({ multiplier, startgame }) => {
   const animationRef = useRef(null);
   const multiplierRef = useRef(null);
+  const timerRef = useRef(null);
+  const [timer, setTimer] = useState(10);
+
+  const [multiplierValue, setMultiplier] = useState(0);
+
   const [currentMultiplier, setCurrentMultiplier] = useState(0);
 
+  console.log("Game Multiplier Value");
+
   const checkGameStatus = async () => {
-    const contractAddress = "0x54c512d21c71a8f6f6edD45A23fA9196c63f825d";
+    const contractAddress = "0xD7746beC4f562b4c6eD610417858f97175Fe6854";
     const provider = new ethers.providers.Web3Provider(window.ethereum);
     const accounts = await provider.listAccounts();
     const signer = provider.getSigner();
     const network = await provider.getNetwork();
-
-    console.log("Call Game Status");
 
     const contract = new ethers.Contract(
       contractAddress,
@@ -25,7 +32,6 @@ const GameSpace = () => {
     );
 
     try {
-      await contract.startNewGame();
       const getGameStatus = await contract.getGameStatus();
       const contractResponse = await contract.checkGameStatus();
       console.log(contractResponse);
@@ -35,58 +41,86 @@ const GameSpace = () => {
   };
 
   useEffect(() => {
-    checkGameStatus();
-    animationRef.current = anime({
-      targets: ".spacecraft",
-      translateX: 720,
-      delay: function (el, i) {
-        return i * 1000;
-      },
-      direction: "normal",
-      easing: "easeInOutSine",
-      complete: function () {
-        animationRef.current = anime({
-          targets: ".spacecraft",
-          translateY: -60,
-          loop: true,
-          delay: function (el, i) {
-            return i * 100;
-          },
-          direction: "alternate",
-          easing: "easeInOutSine",
-          complete: function () {
-            console.log("Animation Completed");
-          },
-        });
-      },
-    });
-    multiplierRef.current = anime({
-      targets: ".multiplier-text",
-      textContent: 400,
-      easing: "easeInOutQuad",
-      round: 1 / 5,
-      duration: 5000 ? 5000 : 4000,
-    });
-  }, []);
-  return (
-    <div
-      style={{ height: 500 }}
-      className="border-2 text-yellow-400 relative h-96 mx-10 my-10  overflow-hidden rounded-lg border-yellow-400"
-    >
-      <video autoPlay="true" muted id="gamebackground">
-        <source src="/bge.mp4" type="video/mp4" />
-      </video>
+    setMultiplier(multiplier);
+    if (!startgame) {
+      timerRef.current = anime({
+        targets: ".timer-text",
+        textContent: 0,
+        easing: "easeInOutQuad",
+        round: 1,
+        duration: 10000,
+      });
+    } else {
+      animationRef.current = anime({
+        targets: ".spacecraft",
+        translateX: 720,
+        delay: function (el, i) {
+          return i * 1000;
+        },
+        direction: "normal",
+        easing: "easeInOutSine",
+        complete: function () {
+          animationRef.current = anime({
+            targets: ".spacecraft",
+            translateY: -60,
+            loop: true,
+            delay: function (el, i) {
+              return i * 100;
+            },
+            direction: "alternate",
+            easing: "easeInOutSine",
+            complete: function () {
+              console.log("Animation Completed");
+            },
+          });
+        },
+      });
+      multiplierRef.current = anime({
+        targets: ".multiplier-text",
+        textContent: multiplier,
+        endDelay: 20000,
+        easing: "easeInOutQuad",
+        round: 2,
+        duration: 10000,
+      });
+    }
+  }, [startgame]);
 
-      <div className="spacecraft">
-        <SpaceCraft />
-      </div>
-      <div className="absolute multiplier flex items-center top-72">
-        <span className="font-VT323 multiplier-text text-6xl text-white">
-          {currentMultiplier}
-        </span>
-        <span className="ml-1 text-6xl font-VT323">x</span>
-      </div>
-    </div>
+  return (
+    <>
+      {startgame ? (
+        <div
+          style={{ height: 500 }}
+          className="border-2 text-yellow-400 relative h-96 mx-10 my-10  overflow-hidden rounded-lg border-yellow-400"
+        >
+          <video autoPlay="true" loop="true" muted id="gamebackground">
+            <source src="/bge.mp4" type="video/mp4" />
+          </video>
+
+          <div className="spacecraft">
+            <SpaceCraft />
+          </div>
+          <div className="absolute multiplier flex items-center top-72">
+            <span className="font-VT323 multiplier-text text-6xl text-white">
+              {currentMultiplier}
+            </span>
+            <span className="ml-1 text-6xl font-VT323">x</span>
+          </div>
+        </div>
+      ) : (
+        <div
+          style={{ height: 500 }}
+          className="border-2 text-yellow-400 relative h-96 mx-10 my-10 flex items-center justify-center  overflow-hidden rounded-lg border-yellow-400"
+        >
+          <div className="w-48 h-48">
+            <Lottie animationData={RocketAnimation} loop={true} />
+            <span className="text-green-400 font-VT323 text-2xl font-bold">
+              Round starts in <span className="timer-text">{timer}</span>
+            </span>
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
