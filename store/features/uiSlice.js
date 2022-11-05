d0e72315af5a8993778728380e76a933ab324670
moganesan <developer.moganesan>
2022-11-05 21:57:10 +0530

    redux

--- store/features/uiSlice.js
+++ store/features/uiSlice.js
@@ -2,12 +2,19 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const gameState = {
   startGame: false,
+  currentMultiplier: 0,
 };
 
 export const gameSlice = createSlice({
   name: "game",
   initialState: gameState,
   reducers: {
+    setMultiplier: (state, actions) => {
+      state.currentMultiplier = actions.payload;
+    },
+    resetMultiplier: (state, actions) => {
+      state.currentMultiplier = 0;
+    },
     startGame: (state, action) => {
       state.startGame = true;
     },
@@ -19,6 +26,7 @@ export const gameSlice = createSlice({
 
 const { reducer } = gameSlice;
 
-export const { startGame, stopGame } = gameSlice.actions;
+export const { startGame, stopGame, setMultiplier, resetMultiplier } =
+  gameSlice.actions;
 
 export default reducer;
